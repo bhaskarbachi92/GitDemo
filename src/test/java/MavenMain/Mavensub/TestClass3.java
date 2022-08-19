546cdacbfda6f85a32bd1495d14a7315412e5966
bhaskar <bhaskerbachi92@gmail.com>
2022-08-19 07:54:50 +0530

    switched to new branch

--- src/test/java/MavenMain/Mavensub/TestClass3.java
+++ src/test/java/MavenMain/Mavensub/TestClass3.java
@@ -5,10 +5,12 @@ public class TestClass3 {
 	public void firsttest()
 	{
 	 System.out.println("Firsttest of Testclass3");
+	 System.out.println("second test of Testclass3");
 	}
 	public void secondtest()
 	{
 	 System.out.println("Firsttest of Testclass3");
+	 System.out.println("New branch created");
 	}
 
 }
