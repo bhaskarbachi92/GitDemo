5e4512c0f6397d4d1832ba5cd7af53d260e19fcd
bhaskar <bhaskerbachi92@gmail.com>
2022-08-19 07:12:48 +0530

    Modified Testclass1

--- src/test/java/MavenMain/Mavensub/Testclass1.java
+++ src/test/java/MavenMain/Mavensub/Testclass1.java
@@ -5,6 +5,7 @@ public class Testclass1 {
 	public void firsttest()
 	{
 	 System.out.println("Firsttest of Testclass1");
+	 System.out.println("Secondtest Git push");
 	}
 	
 	public void secondtest()
